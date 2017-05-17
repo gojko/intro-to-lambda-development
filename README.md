c9cb454f9edcd949a946be09c8081f3475781d47
Gojko Adzic <gojko@gojko.com>
2017-05-17 02:18:37 +0200

    solution for task 1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# intro-to-lambda-development
+# Intro to Lambda development
+
+## Useful links
+
+### Task 1
+
+* https://github.com/claudiajs/claudia-api-builder/blob/master/docs/api.md
+* https://claudiajs.com
+
+## Task 2
+
+* http://jmespath.org/examples.html
+* https://claudiajs.com/tutorials/aws-cli-tricks.html
--- solutions/web-api/api.js
+++ solutions/web-api/api.js
@@ -1,9 +1,12 @@
+/*global module, require, console */
+/*eslint strict: ["error", "global"] */
+'use strict';
 const API = require('claudia-api-builder'),
 	api = new API();
 
 module.exports = api;
 
-api.get('/hello', function (request) {
+api.get('/hello', function (/*request*/) {
 	return 'hello world';
 });
 
@@ -15,6 +18,14 @@ api.get('/another', function (request) {
 });
 
 api.get('/greet/{role}/{name}', function (request) {
-	console.log(request);
 	return 'hello ' + request.pathParams.name + ', you are ' + request.pathParams.role;
 });
+
+api.get('/log', function (request) {
+	console.log(request);
+	return 'OK';
+});
+
+api.get('/web-page', function () {
+	return '<html><body><h1>From Lambda</h1></body></html>';
+}, {success: {contentType: 'text/html'}});
