316cdff36908a14feb1fe9986dc2715524835cbd
Gojko Adzic <gojko@gojko.com>
2018-06-11 07:39:04 +0200

    Save-to-s3 solution with stage post

--- solutions/save-to-s3/api.js
+++ solutions/save-to-s3/api.js
@@ -4,22 +4,23 @@ const API = require('claudia-api-builder'),
 	AWS = require('aws-sdk'),
 	s3 = new AWS.S3(),
 	api = new API(),
-	formText = `<html><body>
-		<h1>Register</h1>
-		<form method="post" action="#">
-			<input placeholder="name" name="name" required/><br/>
-			<input type="radio" name="food" value="meat" checked><label>Love meat</label></input><br/>
-			<input type="radio" name="food" value="veg"><label>Hate meat</label></input><br/>
-			<input type="submit" name="SUBMIT"/>
-		</form>
-		</body></html>`,
 	thanksText = `<html><body>
 		<h1>Thanks!</h1>
 		</body></html>`;
 
 module.exports = api;
 
-api.get('/', function () {
+api.get('/', function (request) {
+	const formText = `<html><body>
+		<h1>Register</h1>
+		<form method="post" action="${request.context.stage}/">
+			<input placeholder="name" name="name" required/><br/>
+			<input type="radio" name="food" value="meat" checked><label>Love meat</label></input><br/>
+			<input type="radio" name="food" value="veg"><label>Hate meat</label></input><br/>
+			<input type="submit" name="SUBMIT"/>
+		</form>
+		</body></html>`;
+
 	return formText;
 }, {success: {contentType: 'text/html'}});
 
